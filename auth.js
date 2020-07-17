f5261e4f29890bf9b1d1200dd8f58b75ee66738a
Gautham Guganesh <gauthamguganesh@Gauthams-MacBook-Air.local>
2020-07-17 16:03:51 +0530

    +1

--- auth.js
+++ auth.js
@@ -71,7 +71,7 @@ module.exports = (app, db) => {
             $set: { last_login: new Date() },
             $inc: { login_count: 1 }
           },
-          {upsert: true, new: true},
+          {upsert: true, returnOriginal: false},
           (err, user) => {
               if(err) return console.error(err);
 
