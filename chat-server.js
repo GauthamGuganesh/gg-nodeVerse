e8da9c95ad9fc5519870c94ba20a4cd80cf1f45b
Gautham Guganesh <gauthamguganesh@Gauthams-MacBook-Air.local>
2020-07-18 15:31:14 +0530

    +1.

--- chat-server.js
+++ chat-server.js
@@ -41,7 +41,15 @@ mongo.connect(DATABASE, {useNewUrlParser: true, useUnifiedTopology: true}, (err,
        http.listen(process.env.PORT || 3000);
 
        io.on('connection', (socket) => {
-         console.log('Connected');
+         console.log(socket);
+         var currentUsers = 0;
+         ++currentUsers;
+         console.log("user count :: " + currentUsers);
+         io.emit('user count', currentUsers);
+
+         socket.on('disconnect', () => {
+           console.log("disconnected");
+         })
        })
 
      }
--- public/client.js
+++ public/client.js
@@ -1,8 +1,32 @@
+
 var sendRequest = () => {
     let req = new XMLHttpRequest();
     req.open('GET', "https://heroku-advanced-node.herokuapp.com/chat", true);
     req.onreadystatechange = () => {
-      if(this.readyState == 4 && this.status == 200) document.write(this.responseText)
+      if(req.readyState == 4 && req.status == 200)
+      {
+         document.write(req.responseText);
+
+         var socket = io.connect('https://heroku-advanced-node.herokuapp.com/');
+         socket.on('user count', (data) => {
+           console.log(data);
+         });
+
+         var socket1 = io.connect('https://heroku-advanced-node.herokuapp.com/');
+         socket1.on('user count', (data) => {
+           console.log(data);
+         });
+
+         var socket2 = io.connect('https://heroku-advanced-node.herokuapp.com/');
+         socket2.on('user count', (data) => {
+           console.log(data);
+         });
+
+         var socket3 = io.connect('https://heroku-advanced-node.herokuapp.com/');
+         socket3.on('user count', (data) => {
+           console.log(data);
+         });
+      }
     }
     req.send();
 }
